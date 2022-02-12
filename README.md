160dcd34217f6b6ddf158f56f51c263b8504dca7
h-nriquevieira <h.nriquevieira@gmail.com>
2022-02-12 12:36:16 -0300

    Create initial structure and planning

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+Projecto from Odin Project's JavaScript Basics course.
+
+First version will be a simple text only game, a GUI will only be added in the future.
---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+/*   PLANNING
+
+Create variable for computer play
+Create variable for user play
+Create variable for computer score
+Create variable for user score
+Create variable for rounds played
+Create variable for game to keep going
+
+Create function for a single round
+    [F] Generate computer play and store it in a variable
+    [F] Ask for user play and store it in a variable
+    [F] Compare computer and user play
+            Determine winner
+    Display winner
+    [F] Add score to corresponding variable
+    [F] Display current score
+    Increase rounds counter
+    [F] Display round number out of 5
+
+Create function for rounds checking
+    Check if computer score is >= 3
+    Computer wins
+    Check if user score is >= 3
+    User wins
+    If anyone wins, return information to end game
+
+Create loop to play game
+    Run round
+    Run rounds checker
+
+*/
